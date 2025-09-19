da02587ee96fe633e685e585ae507f455b996a62
Deiva-12 <deivanaii0405@gmail.com>
2025-09-19 15:42:00 +0530

    logging added

--- quote_project/api_handler.py
+++ quote_project/api_handler.py
@@ -1,5 +1,11 @@
 # api_handler.py
 import requests
+import logging
+
+logging.basicConfig(level=logging.INFO,
+                    filename= "api_file.log",
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
 def get_random_quote():
     """Fetches a random quote from the quotable.io API."""
@@ -7,11 +13,15 @@ def get_random_quote():
         response = requests.get("https://quotes-api-self.vercel.app/quote")
         # Raise an exception for bad status codes (4xx or 5xx)
         response.raise_for_status()
+        #print(response)
+        logger.info(response)
 
         data = response.json()
         author = data['author']
         quote = data['quote']
+        logger.info(author)
         return f'"{author}" - {quote}'
         # return data
     except requests.exceptions.RequestException as e:
         return f"Error: Could not fetch a quote. Please check your internet connection. Details: {e}"
+    
--- quote_project/main.py
+++ quote_project/main.py
@@ -1,5 +1,10 @@
 # main.py
+import logging
 from api_handler import get_random_quote
+
+# logging.basicConfig(level=logging.INFO,
+#                     filename="main.log",
+#                     format=' %(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 
 # TODO: Import the get_random_quote function from the api_handler file.
@@ -11,4 +16,5 @@ quote = get_random_quote()
 
 # Print the final quote
 print("--- Your Inspirational Quote of the Day ---")
-print(quote) # TODO: Uncomment this line after you've defined the 'quote' variable
+print(quote)
+logging.info("Main application is finished") # TODO: Uncomment this line after you've defined the 'quote' variable
